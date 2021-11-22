5f57362e13c1311cab3c5f4d2db515f86aee65f6
Tasfiat-dev <69678164+Tasfiat-dev@users.noreply.github.com>
2021-11-22 20:37:39 +0600

    SQLite to Postgres

--- MiakiNp/settings.py
+++ MiakiNp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'MiakiNp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test',
+        'USER': 'postgres',
+        'PASSWORD': 'critic%^&765',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
